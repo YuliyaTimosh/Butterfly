416168ce57fca071f3b6c1e564f18e36eb58b034
yuliia <yuliiatimosh@ukr.net>
2019-04-21 20:43:20 +0300

    add flowers

--- src/app/components/flower/flower.component.ts
+++ src/app/components/flower/flower.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlowerComponent implements OnInit {
 
+name:string;
+high:number;
+family:string;
+colors:Colors;
+options:string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.flowerSelect('chamomilla');
   }
 
-}
+    flowerSelect(flowerName) {
+      if(flowerName == 'chamomilla') {
+      this.name = ' Ромашка';
+      this.high = 15 + ' см';
+      this.family = ' Хрестоцвіті';
+      this.colors = {
+        head: ' білий',
+        stem: ' зелений',
+        leaf: ' зелений'
+      };
+      this.options = ["Вигляд", "Запах", "Термін стояння"];
+    } else if (flowerName == 'rose') {
+      this.name = ' Троянда';
+      this.high = 25 + ' см';
+      this.family = ' Розові';
+      this.colors = {
+        head: ' червоний, білий, жовтий',
+        stem: ' світло-зелений',
+        leaf: ' темно-зелений'
+      };
+      this.options = ["Вигляд", "Запах", "Термін стояння"];
+    } else {
+      this.name = ' Лілія';
+      this.high = 40 + ' см';
+      this.family = ' Лілійні';
+      this.colors = {
+        head: ' жовтий',
+        stem: ' зелений',
+        leaf: ' салатовий'
+      };
+      this.options = ["Вигляд", "Запах", "Термін стояння"];
+
+
+    }
+
+
+    }
+
+
+
+
+  }
+
+  interface Colors {
+    head:string;
+    stem:string;
+    leaf:string;
+  }
