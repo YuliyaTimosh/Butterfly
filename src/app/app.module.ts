09745869dab6cf593a1377da408cbcb28b8e14d1
YuiliaTimosh <yuliyatimosh96@ukr.net>
2019-04-21 17:48:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlowerComponent } from './components/flower/flower.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlowerComponent
   ],
   imports: [
     BrowserModule,
