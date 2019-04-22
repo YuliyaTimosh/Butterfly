8c6ae9a8d09c7e239e182df18ae35d525d405e45
yuliia <yuliiatimosh@ukr.net>
2019-04-22 22:05:30 +0300

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlowerComponent } from './components/flower/flower.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+const appRoutes: Routes = [
+{path: '', component:FlowerComponent},
+{path: 'about', component:ContactsComponent}
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    FlowerComponent
+    FlowerComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/flower/flower.component.ts
+++ src/app/components/flower/flower.component.ts
@@ -12,6 +12,7 @@ high:number;
 family:string;
 colors:Colors;
 options:string[];
+isEdit: boolean = false;
 
   constructor() { }
 
@@ -19,10 +20,28 @@ options:string[];
     this.flowerSelect('chamomilla');
   }
 
+showEdit() {
+this.isEdit = !this.isEdit;
+}
+
+deleteOpt(option) {
+for(let i = 0; i < this.options.length; i++) {
+if(this.options[i] == option)
+this.options.splice(i, 1);
+
+}
+}
+
+addOpt(option) {
+this.options.unshift(option);
+return false;
+
+}
+
     flowerSelect(flowerName) {
       if(flowerName == 'chamomilla') {
       this.name = ' Ромашка';
-      this.high = 15 + ' см';
+      this.high = 15;
       this.family = ' Хрестоцвіті';
       this.colors = {
         head: ' білий',
@@ -32,7 +51,7 @@ options:string[];
       this.options = ["Вигляд", "Запах", "Термін стояння"];
     } else if (flowerName == 'rose') {
       this.name = ' Троянда';
-      this.high = 25 + ' см';
+      this.high = 25;
       this.family = ' Розові';
       this.colors = {
         head: ' червоний, білий, жовтий',
@@ -42,7 +61,7 @@ options:string[];
       this.options = ["Вигляд", "Запах", "Термін стояння"];
     } else {
       this.name = ' Лілія';
-      this.high = 40 + ' см';
+      this.high = 40;
       this.family = ' Лілійні';
       this.colors = {
         head: ' жовтий',
